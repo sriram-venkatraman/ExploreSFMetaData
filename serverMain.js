113e0a688331532655605160039cd6e7d0f3612d
Sriram Venkatraman <sriram.venkatraman@salesforce.com>
2018-09-18 23:51:52 -0400

    debug

--- serverMain.js
+++ serverMain.js
@@ -80,6 +80,15 @@ server.on('request', function (req, res) {
           });  
         res.end();
         break;
+      default:
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Request-Method', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+        res.setHeader('Access-Control-Allow-Headers', '*');
+        res.writeHead(404);
+        res.write('Contents you are looking are Not Found');
+        res.end();
+        break;
     }
   }
 });
